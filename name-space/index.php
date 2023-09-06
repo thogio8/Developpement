e565bf81f0f990bb7979142fa298f65c2685e4f3
Thomas Gioana <thomas.gioana@gmail.com>
2023-09-06 09:07:43 +0200

    commit

--- name-space/index.php
+++ name-space/index.php
@@ -5,21 +5,9 @@ use App\Equipe;
 use App\Championnat;
 use App\dossier1\Personne;
 
+
 $ligue1 = new Championnat("Ligue 1");
-echo $ligue1->getNom();
 
-$psg = new Equipe("PSG", "Luis Enrique", "1970");
-$om = new Equipe("OM", "Marcelino", "1899");
-
-$ligue1->ajouterEquipe($psg);
-$ligue1->ajouterEquipe($om);
-echo PHP_EOL;
-echo $ligue1->getNombreEquipes() . PHP_EOL;
-
-foreach ($ligue1->getEquipes() as $equipe){
-    echo $equipe->getNom() . PHP_EOL;
-}
+$ligue1->ajouterEquipesCSV("./csv/Equipes.csv");
 
 dump($ligue1);
-
-$personne = new Personne("toto");
--- name-space/src/Championnat.php
+++ name-space/src/Championnat.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App;
+
+use League\Csv\Reader;
+
 class Championnat{
     private string $nom;
     /**
@@ -23,6 +26,23 @@ class Championnat{
      */
     public function ajouterEquipe(Equipe $equipe) : void{
         $this->equipes[] = $equipe;
+    }
+
+    /**
+     * @param string $path
+     * @return void
+     */
+    public function ajouterEquipesCSV(string $path) : void{
+        $csv = Reader::createFromPath($path, 'r');
+        $csv->setHeaderOffset(0);
+        $csv->setDelimiter(";");
+
+        $records = $csv->getRecords();
+
+        foreach($records as $record){
+            $equipe = new Equipe($record["Nom"], $record["Entraineur"], $record["Annee"]);
+            $this->equipes[] = $equipe;
+        }
     }
 
     /**
